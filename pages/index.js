10479ae9835191a29264c95c68d30b252c795b85
thefidanabdulla <thefidanabdulla@gmail.com>
2022-10-30 17:03:05 +0400

    Implementing server side rendering

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ import Banner from '../components/Banner'
 import Header from '../components/Header'
 import styles from '../styles/Home.module.css'
 
-export default function Home() {
+export default function Home({ exploreData }) {
   return (
     <div className=''>
       <Head>
@@ -17,8 +17,21 @@ export default function Home() {
       <main className='max-w-7xl mx-auto sm:px-16'>
         <section>
           <h2 className='text-4xl font-semibold mb-5'>Explore Nearby</h2>
+          {exploreData?.map((item) => (
+            <h1 key={item.location}>{item?.location}</h1>
+          ))}
         </section>
       </main>
     </div>
   )
+}
+
+export async function getStaticProps() {
+  const exploreData = await fetch("https://www.jsonkeeper.com/b/4G1G")
+    .then((res) => res.json());
+  return {
+    props: {
+      exploreData
+    }
+  }
 }
